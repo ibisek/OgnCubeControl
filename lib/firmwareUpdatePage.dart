fd8b539146e7b9bd2eb6d7094828a49504046ec8
ibisek <ibisek@protonmail.com>
2020-02-23 17:34:37 +0100

    work in progress ;)

--- lib/firmwareUpdatePage.dart
+++ lib/firmwareUpdatePage.dart
@@ -6,12 +6,13 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 import 'package:cube_control/firmware.dart';
+import 'package:cube_control/btManager.dart';
 
 
 class FirmwareUpdatePage extends StatefulWidget {
   FirmwareUpdatePage({Key key, this.title}) : super(key: key);
 
-  static const routeName = '/extractArguments';
+  static const routeName = '/firmwareUpdate';
 
   final String title;
 
@@ -21,37 +22,57 @@ class FirmwareUpdatePage extends StatefulWidget {
 
 class _FirmwareUpdatePageState extends State<FirmwareUpdatePage> {
 
-  var logArea = TextEditingController();
-
-  List<DropdownMenuItem<String>> dropDownMenuItems;
-  String dropDownMenuSelectedItem;
+  var terminal = TextEditingController();
+  bool btDeviceConnected = true;
   List<BluetoothDevice> pairedBtDevices;
 
   @override
   void initState() {
     super.initState();
-    dropDownMenuItems = getDropDownMenuItems();
-    dropDownMenuSelectedItem = dropDownMenuItems[0].value;
+    btDeviceConnected = BTManager().isConnected();
+//    logArea.text += "Target type: ${firmware.type}\nRelease date: ${firmware.date}\nFile name: ${firmware.filename}";
   }
 
-  List _cities = ["Cluj-Napoca", "Bucuresti", "Timisoara", "Brasov", "Constanta"];
+  void onDeleteIconClick() {
+    terminal.clear();
+  }
 
-  List<DropdownMenuItem<String>> getDropDownMenuItems() {
-    List<DropdownMenuItem<String>> items = new List();
-    for (String city in _cities) {
-      items.add(new DropdownMenuItem(
-          value: city,
-          child: new Text(city)
-      ));
+  void onConnectIconClick(context) async {
+    if(btDeviceConnected) {
+      BTManager().disconnect();
+
+    } else {
+      if (BTManager().selectedDevice == null) {
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text("No bluetooth device selected!"),
+          action: SnackBarAction(
+            label: 'Fix it!',
+            onPressed: () { print('## GO TO SHOW DEVICES SCREEN ##'); /*TODO navigate to device select screen*/ },
+            ),
+          ),
+        );
+
+        return;
+      }
+
+      bool res = await BTManager().connectTo(BTManager().selectedDevice);
+      if (!res) {
+        Fluttertoast.showToast(
+          msg: "Connection failed",
+          toastLength: Toast.LENGTH_SHORT,
+        );
+      }
     }
-    return items;
+
+    // flip the state:
+    setState(() {
+      btDeviceConnected = !btDeviceConnected;
+    });
   }
 
-  void onDropDownItemChanged(String selectedCity) {
-    print("Selected city $selectedCity, we are going to refresh the UI");
-    setState(() {
-      dropDownMenuSelectedItem = selectedCity;
-    });
+  void onHelpIconClick() {
+    print("Heeelp !!");
+    // TODO popup okno s instrukcema co, jak a proc to tak funguje
   }
 
   @override
@@ -63,126 +84,134 @@ class _FirmwareUpdatePageState extends State<FirmwareUpdatePage> {
         .arguments;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(6),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "Target type: ${firmware.type}\nRelease date: ${firmware
-                          .date}\nFile name: ${firmware
-                          .filename}\nurl: ${firmware.url}",
-                      textAlign: TextAlign.left, textScaleFactor: 1.2,),
-                    Text("${firmware.type}", textAlign: TextAlign.left),
-                  ],
-                ),
-                DropdownButton<String> (
-                    value: dropDownMenuSelectedItem,
-                    items: dropDownMenuItems,
-                    onChanged: onDropDownItemChanged,
-                    icon: Icon(Icons.keyboard_arrow_down),
-                    iconSize: 36,
-                    elevation: 20,
-//                    style: TextStyle(
-//                        color: Colors.deepPurple
-//                    ),
-//                    underline: Container(
-//                      height: 2,
-//                      color: Colors.deepPurpleAccent,
-//                    ),
-                ),
-                Card(
-                  color: Colors.white24,
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          Builder(  // this is here to get the right 'context' for the onPressed action
+            builder: (context) =>
+              Center(
+                child: IconButton(
+                    icon: Image(
+                      image: btDeviceConnected ? AssetImage('assets/images/ic_connected.png') : AssetImage('assets/images/ic_disconnected.png'),
+                    ),
+                    tooltip: 'bt device connection status',
+                    onPressed: () => onConnectIconClick(context),
+                  ),
+              ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            tooltip: 'clear log area',
+            onPressed: onDeleteIconClick,
+          ),
+          IconButton(
+            icon: const Icon(Icons.help_outline),
+            tooltip: 'instructions',
+            onPressed: onHelpIconClick,
+          ),
+        ],
+      ),
+
+      body: Center(
+        child: Container(
+//          padding: const EdgeInsets.all(6),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+//              Row(
+//                children: <Widget>[
+//                  Text(
+//                    "Target type: ${firmware.type}\nRelease date: ${firmware
+//                        .date}\nFile name: ${firmware.filename}",
+//                    softWrap: true,
+//                    textAlign: TextAlign.left,
+//                    textScaleFactor: 1.0,),
+//                ],
+//              ),
+              Expanded(
+                child: Container(
+//                  color: Colors.black,
                   child: Padding(
-                    padding: EdgeInsets.all(6),
-                    child: TextField(
-                      maxLines: 16,
-                      decoration: InputDecoration.collapsed(
-                          hintText: "Progress will appear here.."),
-                      controller: logArea,
+                    padding: EdgeInsets.all(8),
+                    child: SingleChildScrollView(
+                      child: TextField(
+                        controller: terminal,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null, //grow automatically
+                        textAlign: TextAlign.left,
+                        decoration: InputDecoration.collapsed(
+                            hintText: "Info about update process will appear here.."
+                        ),
+                        style: TextStyle(color: Colors.green, /*fontFamily: 'Courier',*/),
+                      ),
                     ),
                   ),
                 ),
-                Row(
-                  children: <Widget>[
-                    RaisedButton(
-                      onPressed: () {
-                        logArea.clear();
-                      },
-                      child: Text('[ CLEAR ]'),
-                      padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                    ),
-                    RaisedButton(
-                      //onPressed: showResults,
-                      child: Text('[ SHOW ]'),
-                      padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                    ),
-                  ],
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: flashFirmware,
-          tooltip: 'Flash firmware',
-          icon: Icon(Icons.bluetooth_audio),
-          label: Text('Scan'),
-        )
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: flashFirmware,
+        tooltip: 'Flash firmware',
+        icon: Icon(Icons.bluetooth_audio),
+        label: Text('START'),
+      )
     );
   }
 
+  void printToTerminal(String str) {
+    terminal.text += "$str\n";
+  }
+
   void flashFirmware() async {
-    print("doing stuff");
+    if(!btDeviceConnected) {
+      Fluttertoast.showToast(
+        msg: "Not connected!",
+        toastLength: Toast.LENGTH_SHORT,
+      );
 
-    logArea.text = "Scanning..\n";
-
-    var btAvailable = await FlutterBluetoothSerial.instance.isAvailable;
-    print("btAvailable: $btAvailable");
-    if(btAvailable) {
-      pairedBtDevices = await FlutterBluetoothSerial.instance.getBondedDevices();
-      print("LLL: $pairedBtDevices");
-      for(BluetoothDevice dev in pairedBtDevices) {
-        logArea.text += "${dev.address} -> ${dev.name}\n";
-      }
-
-      BluetoothDevice dev = pairedBtDevices[2];
-
-      if(!dev.isConnected) {
-        try {
-          BluetoothConnection conn = await BluetoothConnection.toAddress(dev.address);
-          print("isConnected: ${conn.isConnected}");
-
-          conn.input.listen((data) {
-            String strData = "";
-            for (var i in data) strData += String.fromCharCode(i);
-            print("data: $strData");
-            logArea.text += strData;
-
-          }).onDone(() {
-            print("Disconnected by remote peer");
-          });
-
-//          conn.output.add([0,1,3,4]); // TOTO posilani dat
-
-        } catch (exception) {
-          print('Error when connecting to device');
-        }
-      }
+      return;
     }
+
+    printToTerminal("All right, hold my beer..\n");
+
+    // TODO flash process in a background thread..
+
+//    var btAvailable = await FlutterBluetoothSerial.instance.isAvailable;
+//    print("btAvailable: $btAvailable");
+//    if(btAvailable) {
+//      pairedBtDevices = await FlutterBluetoothSerial.instance.getBondedDevices();
+//      print("LLL: $pairedBtDevices");
+//      for(BluetoothDevice dev in pairedBtDevices) {
+//        logArea.text += "${dev.address} -> ${dev.name}\n";
+//      }
+//
+//      BluetoothDevice dev = pairedBtDevices[2];
+//
+//      if(!dev.isConnected) {
+//        try {
+//          BluetoothConnection conn = await BluetoothConnection.toAddress(dev.address);
+//          print("isConnected: ${conn.isConnected}");
+//
+//          conn.input.listen((data) {
+//            String strData = "";
+//            for (var i in data) strData += String.fromCharCode(i);
+//            print("data: $strData");
+//            logArea.text += strData;
+//
+//          }).onDone(() {
+//            print("Disconnected by remote peer");
+//          });
+//
+////          conn.output.add([0,1,3,4]); // TOTO posilani dat
+//
+//        } catch (exception) {
+//          print('Error when connecting to device');
+//        }
+//      }
+//    }
   }
 
 } // ~ class
