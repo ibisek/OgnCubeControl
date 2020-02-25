261a1cf5929833f086a41df5fd54dba191634f4e
ibisek <ibisek@protonmail.com>
2020-02-25 10:22:44 +0100

    +crc and binary file len in the json

--- lib/firmware.dart
+++ lib/firmware.dart
@@ -8,12 +8,14 @@ class Firmware {
   final String notes;
   final String filename;
   final String url;
+  final int crc;
+  final int len;
 
   int timestamp;  // unix ts [s]
   bool isStoredLocally = false; // indicates this file needs to be downloaded
   Uint8List bytes;  // firmware bytes
 
-  Firmware({this.type, this.date, this.title,  this.notes, this.filename, this.url});
+  Firmware({this.type, this.date, this.title,  this.notes, this.filename, this.url, this.crc, this.len});
 
   factory Firmware.fromJson(Map<String, dynamic> json) {
     return Firmware(
@@ -23,9 +25,33 @@ class Firmware {
       notes: json['notes'],
       filename: json['filename'],
       url: json['url'],
+      crc: json['crc'],
+      len: json['len'],
     );
   }
 
+  /// @return CRC as XOR of the bytes
+  int calcCrc(Uint8List bytes) {
+    int crc = 0;
+    for (int i=0; i<bytes.lengthInBytes; i++) {
+      crc = crc ^ bytes[i];
+    }
+
+    return crc;
+  }
+
+  /// @param bytes of the firmware's .bin file
+  void setBytes(Uint8List bytes) {
+    this.bytes = bytes;
+
+    int calculatedCrc = calcCrc(bytes);
+
+    if(crc != calculatedCrc || len != bytes.lengthInBytes) {
+      throw Exception("CRC ($crc vs. $calculatedCrc}) or LEN ($len vs. ${bytes.lengthInBytes}) do not match!");
+    }
+  }
+
+  /// @return unix timestamp in [ms]
   int getTs() {
     if (timestamp == null)
       timestamp = DateTime.parse("$date 00:00:01Z").toUtc().millisecondsSinceEpoch; // ~/ 1000;
