c62d60e3e07e2c04f5fb1b622002d382305d651c
ibisek <ibisek@protonmail.com>
2020-02-26 07:56:00 +0100

    cleanup & formatting

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,10 @@ class MyApp extends StatelessWidget {
       ),
       home: MyHomePage(title: 'Available Firmwares'),
       routes: <String, WidgetBuilder>{
-        FirmwareUpdatePage.routeName: (BuildContext context) => FirmwareUpdatePage(title: 'Firmware Upload'),
-        DeviceListPage.routeName: (BuildContext context) => DeviceListPage(title: 'Paired Devices'),
+        FirmwareUpdatePage.routeName: (BuildContext context) =>
+            FirmwareUpdatePage(title: 'Firmware Upload'),
+        DeviceListPage.routeName: (BuildContext context) =>
+            DeviceListPage(title: 'Paired Devices'),
       },
     );
   }
@@ -49,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   // Observes the app state. Detects application shutdown.
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.paused) {
-      if(BTManager().isConnected()) BTManager().disconnect();
+      if (BTManager().isConnected()) BTManager().disconnect();
     }
   }
 
@@ -70,8 +72,10 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     Row row = Row(
       children: <Widget>[
         Container(
-          padding: const EdgeInsets.only(left: 6, right: 8),   //all(8),
-          child: Icon( firmwares[i].isStoredLocally ? Icons.sd_storage : Icons.cloud_queue),
+          padding: const EdgeInsets.only(left: 6, right: 8), //all(8),
+          child: Icon(firmwares[i].isStoredLocally
+              ? Icons.sd_storage
+              : Icons.cloud_queue),
         ),
         Text("${firmwares[i].type}\n${firmwares[i].date}",
             textAlign: TextAlign.left),
@@ -181,13 +185,17 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
           ListTile(
             leading: Icon(Icons.settings_bluetooth),
             title: Text('Paired devices'),
-            onTap: () { Navigator.of(context).pushNamed(DeviceListPage.routeName); },
+            onTap: () {
+              Navigator.of(context).pushNamed(DeviceListPage.routeName);
+            },
           ),
           ListTile(
             leading: Icon(Icons.system_update_alt),
             title: Text('Firmware updates'),
             selected: true,
-            onTap: () { Navigator.pop(context); }, // close the drawer
+            onTap: () {
+              Navigator.pop(context);
+            }, // close the drawer
           ),
 //          ListTile(
 //            leading: FlutterLogo(size: 40.0),
@@ -217,67 +225,11 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
       ),
       drawer: getDrawer(),
       body: getBody(),
-//      body: Center(
-//        // Center is a layout widget. It takes a single child and positions it
-//        // in the middle of the parent.
-//        child: Column(
-//          // Column is also a layout widget. It takes a list of children and
-//          // arranges them vertically. By default, it sizes itself to fit its
-//          // children horizontally, and tries to be as tall as its parent.
-//          //
-//          // Invoke "debug painting" (press "p" in the console, choose the
-//          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//          // to see the wireframe for each widget.
-//          //
-//          // Column has various properties to control how it sizes itself and
-//          // how it positions its children. Here we use mainAxisAlignment to
-//          // center the children vertically; the main axis here is the vertical
-//          // axis because Columns are vertical (the cross axis would be
-//          // horizontal).
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            Text(
-//              'You have pushed the button this many times:',
-//            ),
-//            Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
-//            Text(
-//              'Ahoj Nufu! :)\nJak se mas?'
-//            ),
-//            MaterialButton(
-//              onPressed: () {},
-//              child: Text('toto je tlacitko'),
-//              padding: EdgeInsets.only(left: 100.0, right: 100.0),
-//            ),
-//          ],
-//        ),
-//      ),
       floatingActionButton: FloatingActionButton(
         onPressed: loadFirmwareList,
         tooltip: 'Reload',
         child: Icon(Icons.cached),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: _decrementCounter,
-//        tooltip:  'Decrement',
-//        child: Icon(Icons.remove),
-//      ),
     );
   }
 }
-
-//Future<Album> fetchAlbum() async {
-//  final response = await http.get('https://jsonplaceholder.typicode.com/albums/2');
-//
-//  if (response.statusCode == 200) {
-//    // If the server did return a 200 OK response, then parse the JSON.
-//    return Album.fromJson(json.decode(response.body));
-//
-//  } else {
-//    // If the server did not return a 200 OK response, then throw an exception.
-//    throw Exception('Failed to load album');
-//  }
-//}
